5d9286351a5dc9cd2d8388b5fcc76c540a7221eb
onlyJinx <925198583@qq.com>
2021-07-26 23:39:33 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -471,7 +471,7 @@ function Up_kernel(){
 
 }
 
-function xray(){
+function install_xray(){
 
 	check_port "XRAY_XTLS 监听端口(默认1000)?  " 1000
 	XRAY_XTLS_PORT=$port
@@ -486,24 +486,25 @@ function xray(){
 	read -p "WebSocks Path(默认 WscokilR39o)?  " XRAY_WS_PATH
 	XRAY_WS_PATH=${XRAY_WS_PATH:-WscokilR39o}
 
-	XRAY_RELEASE_LATEST=`wget -q -O - https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep tag_name|cut -f4 -d "\""|cut -c 2-`
-	#获取github仓库最新版release引用 https://bbs.zsxwz.com/thread-3958.htm
-	wget -P /tmp https://github.com/XTLS/Xray-core/releases/download/v$XRAY_RELEASE_LATEST/Xray-linux-64.zip
-	if ! [[ "$(type -P unzip)" ]];then
-		if [[ "$(type -P apt)" ]];then
-			apt install -y unzip
-		else
-			yum install -y unzip
-		fi
-	else 
-		unzip /tmp/Xray-linux-64.zip -d /tmp
-	fi
-	if ! [[ -d /usr/local/share/xray ]];then
-		mkdir /usr/local/share/xray
-	fi
-	mv /tmp/geoip.dat /usr/local/share/xray/geoip.dat
-	mv /tmp/geosite.dat /usr/local/share/xray/geosite.dat
-	mv /tmp/xray /usr/local/bin/xray
+	bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
+	# XRAY_RELEASE_LATEST=`wget -q -O - https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep tag_name|cut -f4 -d "\""|cut -c 2-`
+	# #获取github仓库最新版release引用 https://bbs.zsxwz.com/thread-3958.htm
+	# wget -P /tmp https://github.com/XTLS/Xray-core/releases/download/v$XRAY_RELEASE_LATEST/Xray-linux-64.zip
+	# if ! [[ "$(type -P unzip)" ]];then
+	# 	if [[ "$(type -P apt)" ]];then
+	# 		apt install -y unzip
+	# 	else
+	# 		yum install -y unzip
+	# 	fi
+	# else 
+	# 	unzip /tmp/Xray-linux-64.zip -d /tmp
+	# fi
+	# if ! [[ -d /usr/local/share/xray ]];then
+	# 	mkdir /usr/local/share/xray
+	# fi
+	# mv /tmp/geoip.dat /usr/local/share/xray/geoip.dat
+	# mv /tmp/geosite.dat /usr/local/share/xray/geosite.dat
+	# mv /tmp/xray /usr/local/bin/xray
 
 	if [[ "$(type -P xray)" ]]; then
 		XRAY_UUID=$(xray uuid)
@@ -527,27 +528,26 @@ function xray(){
 	sed -i 's/GRPC_UUID/$XRAY_GRPC_UUID/' $XRAY_CONFIG
 	sed -i 's/WS_UUID/$XRAY_WS_UUID/' $XRAY_CONFIG
 
-	cat > /etc/systemd/system/xray.service <<-EOF
-	[Unit]
-	Description=Xray Service
-	Documentation=https://github.com/xtls
-	After=network.target nss-lookup.target
+	# cat > /etc/systemd/system/xray.service <<-EOF
+	# [Unit]
+	# Description=Xray Service
+	# Documentation=https://github.com/xtls
+	# After=network.target nss-lookup.target
 
-	[Service]
-	User=root
-	#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-	#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-	#NoNewPrivileges=true
-	ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
-	Restart=on-failure
-	RestartPreventExitStatus=23
-	LimitNPROC=10000
-	LimitNOFILE=1000000
+	# [Service]
+	# User=root
+	# #CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+	# #AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+	# #NoNewPrivileges=true
+	# ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
+	# Restart=on-failure
+	# RestartPreventExitStatus=23
+	# LimitNPROC=10000
+	# LimitNOFILE=1000000
 
-	[Install]
-	WantedBy=multi-user.target
-	EOF
-
+	# [Install]
+	# WantedBy=multi-user.target
+	# EOF
 	systemctl start xray
 
 	check "XRAY服务启动失败" "XRAY服务正在运行"
