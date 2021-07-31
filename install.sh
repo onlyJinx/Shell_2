78cadc7c44b161388454f8ed15a937bc0d8f9df1
onlyJinx <925198583@qq.com>
2021-08-01 01:47:52 +0800

    fix transmission https proxy

--- install.sh
+++ install.sh
@@ -524,7 +524,7 @@ function transmission(){
 						ssl_protocols TLSv1.2 TLSv1.3;
 						ssl_prefer_server_ciphers  on;
 						ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
-						location /${port} {
+						location / {
 							proxy_redirect off;
 							proxy_pass http://127.0.0.1:${port};
 							proxy_http_version 1.1;
