286de266b6171e85150ef0f0a32e27673aa2f818
onlyJinx <925198583@qq.com>
2021-08-07 15:55:12 +0800

    add transmission_file_server error page redirect

--- install.sh
+++ install.sh
@@ -518,6 +518,7 @@ function transmission(){
 		    ssl_session_timeout  5m;
 		    ssl_protocols TLSv1.2 TLSv1.3;
 		    ssl_prefer_server_ciphers  on;
+		    error_page 404 https://${TRANSMISSION_DOMAIN}/${TRRNA_FILE_SERVER_PATH}/;
 		    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
 		    location / {
 		        proxy_redirect off;
